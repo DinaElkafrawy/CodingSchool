62c9d463aea76dbf2b5668eead46282e0701bd55
DinaElkafrawy <dina.elkafrawy@student.guc.edu.eg>
2019-11-22 01:57:18 +0200

    5 scenes merged, additional adjustments required

---
+++ HCI/Assets/LoopsAssets/Scripts/GameFlowIterative.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameFlowIterative : MonoBehaviour
+{
+    private AudioSource sound;
+    public AudioClip end;
+    public GameObject s5;
+    public GameObject s4;
+    public AudioClip click;
+    public AudioClip destroy;
+    public Button next;
+    public GameObject s3;
+    public GameObject s2;
+    public GameObject s1;
+    int counterone = 1;
+    int countertwo = 2;
+    int counterthree = 3;
+    int counterfour = 4;
+    int counterfifth = 5;
+    public Text fiveText;
+    public Text fourText;
+
+    public Text threeText;
+    public Text twoText;
+    public Text oneText;
+    public GameObject done;
+    public Text endgame;
+    bool endd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        done.SetActive(false);
+        fiveText.text = counterfifth.ToString();
+        fourText.text = counterfour.ToString();
+        threeText.text = counterthree.ToString();
+        twoText.text = countertwo.ToString();
+        oneText.text = counterone.ToString();
+        next.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counterone == 0 && countertwo == 0 && counterthree == 0 && counterfour == 0 && counterfifth == 0)
+        {
+            //done.SetActive(true);
+            endgame.text = "Congratulations on passing the while loops !! Now while the challenges are not finished keep playing ;) ";
+            endd = true;
+            next.interactable = true;
+            playend();
+        }
+    }
+
+    public void NextEx()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void playend()
+    {
+        if (endd)
+        {
+            sound.PlayOneShot(end);
+            endd = false;
+        }
+    }
+   public void spherefive()
+    {
+        sound.PlayOneShot(click);
+        counterfifth--;
+        fiveText.text = counterfifth.ToString();
+        if (counterfifth == 0)
+        {
+            sound.PlayOneShot(destroy);
+
+            Destroy(s5);
+        }
+    }
+    public void spherefour()
+    {
+        sound.PlayOneShot(click);
+
+        counterfour--;
+        fourText.text = counterfour.ToString();
+        if (counterfour == 0)
+        {
+            sound.PlayOneShot(destroy);
+            Destroy(s4);
+        }
+    }
+    public void spherethree()
+    {
+        sound.PlayOneShot(click);
+
+        counterthree--;
+        threeText.text = counterthree.ToString();
+        if (counterthree == 0)
+        {
+            sound.PlayOneShot(destroy);
+
+            Destroy(s3);
+        }
+    }
+
+    public void spheretwo()
+    {
+        sound.PlayOneShot(click);
+
+        countertwo--;
+        twoText.text = countertwo.ToString();
+        if (countertwo == 0)
+        {
+            sound.PlayOneShot(destroy);
+
+            Destroy(s2);
+        }
+    }
+    public void sphereone()
+    {
+        sound.PlayOneShot(click);
+
+        counterone--;
+        oneText.text = counterone.ToString();
+        if (counterone == 0)
+        {
+            sound.PlayOneShot(destroy);
+
+            Destroy(s1);
+        }
+    }
+
+}
--- HCI/Assets/SequentialAssets/scripts/Game01.cs
+++ HCI/Assets/SequentialAssets/scripts/Game01.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class Game01 : MonoBehaviour
 {
+    public AudioClip right;
+    public AudioClip wrong;
+    private AudioSource sound;
     bool first=false;
     bool second=false;
     bool third=false;
@@ -24,6 +27,7 @@ public class Game01 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sound = GetComponent<AudioSource>();
         canvas.SetActive(false);
         next.interactable = false;
     }
@@ -43,7 +47,8 @@ public class Game01 : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void Clickone() {
-        Debug.Log("ONE");
+        sound.clip = right;
+        sound.Play();
         first = true;
         firstsphere.SetActive(false);
     }
@@ -53,8 +58,14 @@ public class Game01 : MonoBehaviour
     {
         if (first == true)
         {
+            sound.clip = right;
+            sound.Play();
             second = true;
             secondsphere.SetActive(false);
+        }
+        else {
+            sound.clip = wrong;
+            sound.Play();
         }
     }
 
@@ -62,34 +73,62 @@ public class Game01 : MonoBehaviour
     {
         if (first == true && second ==true)
         {
+            sound.clip = right;
+            sound.Play();
             third = true;
             thirdsphere.SetActive(false);
+        }
+        else
+        {
+            sound.clip = wrong;
+            sound.Play();
         }
     }
     public void Clickfourth()
     {
         if (first == true&&second==true&& third==true)
         {
+            sound.clip = right;
+            sound.Play();
             fourth = true;
             fourthsphere.SetActive(false);
+        }
+        else
+        {
+            sound.clip = wrong;
+            sound.Play();
         }
     }
     public void Clickfifth()
     {
         if (first == true && second == true && third == true && fourth==true)
         {
+            sound.clip = right;
+            sound.Play();
             fifth = true;
             fifthsphere.SetActive(false);
+        }
+        else
+        {
+            sound.clip = wrong;
+            sound.Play();
         }
     }
     public void ClickSixth()
     {
         if (first == true && second == true && third == true && fourth == true && fifth== true)
         {
+            sound.clip = right;
+            sound.Play();
             sixth = true;
             sixthsphere.SetActive(false);
             canvas.SetActive(true);
 
+        }
+        else
+        {
+            sound.clip = wrong;
+            sound.Play();
         }
     }
 }
